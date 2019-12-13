39a1e9ab2f81fa6571c532a1e77e34f447a4433c
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-12-13 02:50:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,22 +18,25 @@ class Transfer
   end 
   
 def execute_transaction
-    if !self.valid? && @sender.balance <= @amount 
-      @status = "rejected" 
-      @status = "Transaction rejected. Please check your account balance."
-    elsif @status == "pending"
-    BankAccount.all.map do | person |
-      if person == @sender 
-        person.balance -= @amount 
-      elsif person == @receiver
-        person.balance += @amount
-      end
+    if @sender.balance < @amount
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+
+    elsif @status == "complete"
+      puts "Transaction was already excuted"
+    else
+      @sender.deposit( @amount * -1 ) 
+      @receiver.deposit( @amount )
+      @status = "complete"
     end
-
-  end
-  @status = "complete"
-
   end
 
-end	
+  def reverse_transfer
+    if @status == "complete"
+      @sender.deposit( @amount ) 
+      @receiver.deposit( @amount * -1)
+      @status = "reversed"
+    end
+  end
 
+end
